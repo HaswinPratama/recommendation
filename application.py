1d60fec684e033a42ae88284dfd27687c63dbfd6
Octgi Ristya Perdana <octgi.ristya.perdana@gmail.com>
2023-04-27 10:16:34 +0700

    Adding initial page

--- application.py
+++ application.py
@@ -9,3 +9,9 @@ app = FastAPI()
 async def read_item(id: str):
     data = dataEng.getData(id)
     return {"id": id, "data": data, 'message': 'Haswin babi!'}
+
+    @app.get("/")
+    def home():
+        return "http://127.0.0.1:8000/api/v1/recomendation/id"
+
+
